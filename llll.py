e62c069335d5b0b557b4af483c82baf63f2379f8
Gosia <you@example.com>
2016-07-07 15:27:52 +0100

    randomisd sample of six numbers removing numbers one by one with a for loop.

---
+++ llll.py
@@ -0,0 +1,26 @@
+from random import randint
+randomnumbers = []
+for y in range (0, 6):
+    y = randint(1,50)
+    randomnumbers.append(y)
+    print(y)
+
+randomnumbers1 = []
+for z in range (0,6):
+
+    try:
+        number = int(raw_input("Choose six numbers and enter them one by one ffrom 1 to 49:"))
+        if not (1 <= number <= 49):
+
+    
+            print "Invalid option, enter numbers form 1 to 49"
+            number = raw_input
+        else:
+            randomnumbers1.append(number)
+            print "Your number is", number
+
+    for i in randomnumbers:
+        for j in randomnumbers1:
+            if i == j:
+                print "number %i matches %i" % (i, j)
+
--- lottery.py
+++ lottery.py
@@ -1,19 +1,25 @@
-from random import randint, shuffle
+#from random import randint, shuffle
 
-def luckyNumbers(rangeLeft, rangeRight, size):
-    """ determines the lucky numbers which win the lottery! """
-    originalRange = range(rangeLeft, rangeRight)
-    shuffle(originalRange)
-    return originalRange[:size]
+#def luckyNumbers(rangeLeft, rangeRight, size):
+   # """ determines the lucky numbers which win the lottery! """
+    #originalRange = range(rangeLeft, rangeRight)
+    #shuffle(originalRange)
+    #return originalRange[:size]
 
-randomnumbers = luckyNumbers(1, 49, 6)
+#randomnumbers = luckyNumbers(1, 49, 6)
+import random
+lotteryrandom = []
+a = range(1,50)
+for i in range(6):
+    b = a[random.randint(0,len(a))]
+    a.remove(b)
+    print b
+    lotteryrandom.append(b)
 
-for y in randomnumbers:
-    print(y)
-
-randomnumbers1 = []
+randomnumbers = []
 
 for z in range (0,6):
+
     try:
         number = int(raw_input("Choose six numbers and enter them one by one from 1 to 49:"))
         if not (1 <= number <= 49):
@@ -22,14 +28,14 @@ for z in range (0,6):
         print "Invalid option, enter numbers from 1 to 49"
         number = raw_input
     else:
-        randomnumbers1.append(number)
+        randomnumbers.append(number)
         print "Your number is", number
 
 
-for i in randomnumbers: 
-    for j in randomnumbers1:
-        if i == j:
-            print "number %i matches %i" % (i, j)
+for i in lotteryrandom: 
+    for z in randomnumbers:
+        if i == z:
+            print "number %i matches %i" % (i, z)
 
 
 
